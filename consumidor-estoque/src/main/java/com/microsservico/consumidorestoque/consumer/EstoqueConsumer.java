8770ac76be4948da908293cf88749c2968f31c88
Geneton Souza <genetonuanderson@gmail.com>
2023-12-04 18:51:30 -0300

    adcionando projeto ao repositorio

---
+++ consumidor-estoque/src/main/java/com/microsservico/consumidorestoque/consumer/EstoqueConsumer.java
@@ -0,0 +1,15 @@
+package com.microsservico.consumidorestoque.consumer;
+import constantes.RabbitMQConstantes;
+import dto.EstoqueDto;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EstoqueConsumer {
+@RabbitListener(queues = RabbitMQConstantes.FILA_ESTOQUE)
+    private void consumidor(EstoqueDto estoqueDto) {
+    System.out.println(estoqueDto.codigoproduto);
+    System.out.println(estoqueDto.quantidade);
+    System.out.println("-----------------------");
+    }
+}
